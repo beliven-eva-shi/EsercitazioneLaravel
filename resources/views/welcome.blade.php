1d2d118dad57117a0fda5a3c5cd92d3a3a61b16d
Eva Shi <eva.shi@beliven.com>
2024-05-20 17:53:40 +0200

    fix: show 'Go to list' only if auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <div><a href="/login"> Log in</a></div>
-<div><a href="/project"> Go to task list</a></div>
+@auth
+    <div><a href="/project"> Go to task list</a></div>
+@endauth
 
 </html>
